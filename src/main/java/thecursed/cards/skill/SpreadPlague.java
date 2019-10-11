1dd464285a8532399c4900e5b3fc578ef007c2b8
Jaehoon Park <solderist.park@gmail.com>
2019-10-12 00:36:17 +0900

    patch v1.1.6 - fix bug that bloody harpoon doesn't work with spread plague

--- src/main/java/thecursed/cards/skill/SpreadPlague.java
+++ src/main/java/thecursed/cards/skill/SpreadPlague.java
@@ -11,8 +11,8 @@ import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 import thecursed.TheCursedMod;
+import thecursed.actions.ApplyBleedAction;
 import thecursed.enums.AbstractCardEnum;
-import thecursed.powers.BleedPower;
 
 public class SpreadPlague extends CustomCard {
     public static final String ID = "TheCursedMod:SpreadPlague";
@@ -44,10 +44,7 @@ public class SpreadPlague extends CustomCard {
         AbstractDungeon.getMonsters().monsters.stream()
                 .filter(mo -> !mo.isDeadOrEscaped())
                 .forEach(mo -> {
-                    AbstractDungeon.actionManager.addToBottom(
-                            new ApplyPowerAction(
-                                    mo, p, new BleedPower(mo, p, this.magicNumber), this.magicNumber, true,
-                                    AttackEffect.NONE));
+                    AbstractDungeon.actionManager.addToBottom(new ApplyBleedAction(mo, p, this.magicNumber));
                     AbstractDungeon.actionManager.addToBottom(
                             new ApplyPowerAction(
                                     mo, p, new VulnerablePower(mo, VULNERABLE_DURATION, false),
